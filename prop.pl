8e1ca232b17801b5ca81d6c830f486b0281e072c
JulietteSCHILLING <juliette.schilling2@etu.univ-lorraine.fr>
2024-04-28 15:02:17 +0200

    ajout des a(i)

--- prop.pl
+++ prop.pl
@@ -4,21 +4,37 @@
 :- op(700, xfy, ou).    % Opérateur 'ou' (disjonction)
 :- op(600, xfy, =>).    % Opérateur '=>' (implication)
 
-% Prédicat transformer/2 pour transformer une formule en un arbre
-transformer(F, F) :-
-    atomic(F). % Cas de base : F est une proposition atomique
+% Prédicat transformer/2 pour transformer une formule en un arbre avec numérotation en profondeur
+transformer(F, Arbre) :-
+    transformer(F, Arbre, 0, _), % Appelle le prédicat avec un numéro initial de nœud à 0
+    write(Arbre).
 
-transformer(non F, ArbreF) :-
-    transformer(F, ArbreF). % Transforme F en sous-arbre ArbreF
+% Cas de base : F est une proposition atomique
+transformer(F, [Numero, F, nil, nil], Numero, Numero) :-
+    atomic(F).
 
-transformer(F1 => F2, [F1 => F2, ArbreF1, ArbreF2]) :-
-    transformer(F1, ArbreF1),
-    transformer(F2, ArbreF2).
+% Cas pour l'opérateur unaire non
+transformer(non F, ArbreF, NumeroIn, NumeroOut) :-
+    NewNumero is NumeroIn + 1,
+    transformer(F, ArbreF, NewNumero, NumeroOut).
 
-transformer(F1 ou F2, [F1 ou F2, ArbreF1, ArbreF2]) :-
-    transformer(F1, ArbreF1),
-    transformer(F2, ArbreF2).
+% Cas pour l'opérateur implication =>
+transformer(F1 => F2, [NumeroIn, F1 => F2, ArbreF1, ArbreF2], NumeroIn, NumeroOut) :-
+    NewNumero1 is NumeroIn + 1,
+    transformer(F1, ArbreF1, NewNumero1, NumeroInter),
+    NewNumero2 is NumeroInter + 1,
+    transformer(F2, ArbreF2, NewNumero2, NumeroOut).
 
-transformer(F1 et F2, [F1 et F2, ArbreF1, ArbreF2]) :-
-    transformer(F1, ArbreF1),
-    transformer(F2, ArbreF2).
+% Cas pour l'opérateur disjonction ou
+transformer(F1 ou F2, [NumeroIn, F1 ou F2, ArbreF1, ArbreF2], NumeroIn, NumeroOut) :-
+    NewNumero1 is NumeroIn + 1,
+    transformer(F1, ArbreF1, NewNumero1, NumeroInter),
+    NewNumero2 is NumeroInter + 1,
+    transformer(F2, ArbreF2, NewNumero2, NumeroOut).
+
+% Cas pour l'opérateur conjonction et
+transformer(F1 et F2, [NumeroIn, F1 et F2, ArbreF1, ArbreF2], NumeroIn, NumeroOut) :-
+    NewNumero1 is NumeroIn + 1,
+    transformer(F1, ArbreF1, NewNumero1, NumeroInter),
+    NewNumero2 is NumeroInter + 1,
+    transformer(F2, ArbreF2, NewNumero2, NumeroOut).
