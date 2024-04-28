2c72bebfecb329326ec2ed222f27f44bd708528d
JulietteSCHILLING <juliette.schilling2@etu.univ-lorraine.fr>
2024-04-28 15:54:05 +0200

    ajout type secondaire

--- prop.pl
+++ prop.pl
@@ -6,54 +6,66 @@
 
 % Prédicat transformer/2 pour transformer une formule en un arbre avec numérotation en profondeur
 transformer(F, Arbre) :-
-    transformer(F, Arbre, 0, _, 0), % Appelle le prédicat avec un numéro initial de nœud à 0
+    transformer(F, Arbre, 0, _, 0, '_'), % Appelle le prédicat avec un numéro initial de nœud à 0
     write(Arbre).
 
 % Cas de base : F est une proposition atomique
-transformer(F, [Numero, Polarite, F, nil, nil], Numero, Numero, Polarite) :-
+transformer(F, [Numero, '_', TypeSec, Polarite, F, nil, nil], Numero, Numero, Polarite, TypeSec) :-
     atomic(F).
 
 % Cas pour l'opérateur unaire 'non'
-transformer(non F, ArbreF, NumeroIn, NumeroOut, Polarite) :-
+transformer(non F, ArbreF, NumeroIn, NumeroOut, Polarite, TypeSec) :-
     InversePolarite is 1 - Polarite,
-    transformer(non F, ArbreF, NumeroIn, NumeroOut, InversePolarite). 
+    transformer(non F, ArbreF, NumeroIn, NumeroOut, InversePolarite, 'alpha1'). 
 
 % Cas pour l'opérateur implication '=>'
-transformer(F1 => F2, [NumeroIn, Type, Polarite, F1 => F2, ArbreF1, ArbreF2], NumeroIn, NumeroOut, Polarite) :-
+transformer(F1 => F2, [NumeroIn, Type, TypeSec, Polarite, F1 => F2, ArbreF1, ArbreF2], NumeroIn, NumeroOut, Polarite, TypeSec) :-
     ( Polarite =:= 0
     ->
-        Type = alpha
+        Type = alpha,
+        TypeSec1 = alpha1,
+        TypeSec2 = alpha2
     ;
-        Type = beta
+        Type = beta,
+        TypeSec1 = beta1,
+        TypeSec2 = beta2
     ),
     NewNumero1 is NumeroIn + 1,
     InversePolarite is 1 - Polarite,
-    transformer(F1, ArbreF1, NewNumero1, NumeroInter, InversePolarite),
+    transformer(F1, ArbreF1, NewNumero1, NumeroInter, InversePolarite, TypeSec1),
     NewNumero2 is NumeroInter + 1,
-    transformer(F2, ArbreF2, NewNumero2, NumeroOut, Polarite).
+    transformer(F2, ArbreF2, NewNumero2, NumeroOut, Polarite, TypeSec2).
 
 % Cas pour l'opérateur disjonction 'ou' (pas de changement de polarite)
-transformer(F1 ou F2, [NumeroIn, Type, Polarite, F1 ou F2, ArbreF1, ArbreF2], NumeroIn, NumeroOut, Polarite) :-
+transformer(F1 ou F2, [NumeroIn, Type, TypeSec, Polarite, F1 ou F2, ArbreF1, ArbreF2], NumeroIn, NumeroOut, Polarite, TypeSec) :-
     ( Polarite =:= 0
     ->
-        Type = alpha
+        Type = alpha,
+        TypeSec1 = alpha1,
+        TypeSec2 = alpha2
     ;
-        Type = beta
+        Type = beta,
+        TypeSec1 = beta1,
+        TypeSec2 = beta2
     ),
     NewNumero1 is NumeroIn + 1,
-    transformer(F1, ArbreF1, NewNumero1, NumeroInter, Polarite),
+    transformer(F1, ArbreF1, NewNumero1, NumeroInter, Polarite, TypeSec1),
     NewNumero2 is NumeroInter + 1,
-    transformer(F2, ArbreF2, NewNumero2, NumeroOut, Polarite).
+    transformer(F2, ArbreF2, NewNumero2, NumeroOut, Polarite, TypeSec2).
 
 % Cas pour l'opérateur conjonction 'et' (pas de changement de polarite)
-transformer(F1 et F2, [NumeroIn, Type, Polarite, F1 et F2, ArbreF1, ArbreF2], NumeroIn, NumeroOut, Polarite) :-
-    ( Polarite =:= 0
+transformer(F1 et F2, [NumeroIn, Type, TypeSec, Polarite, F1 et F2, ArbreF1, ArbreF2], NumeroIn, NumeroOut, Polarite, TypeSec) :-
+    ( Polarite =:= 1
     ->
-        Type = beta
+        Type = alpha,
+        TypeSec1 = alpha1,
+        TypeSec2 = alpha2
     ;
-        Type = alpha
+        Type = beta,
+        TypeSec1 = beta1,
+        TypeSec2 = beta2
     ),
     NewNumero1 is NumeroIn + 1,
-    transformer(F1, ArbreF1, NewNumero1, NumeroInter, Polarite),
+    transformer(F1, ArbreF1, NewNumero1, NumeroInter, Polarite, TypeSec1),
     NewNumero2 is NumeroInter + 1,
-    transformer(F2, ArbreF2, NewNumero2, NumeroOut, Polarite).
+    transformer(F2, ArbreF2, NewNumero2, NumeroOut, Polarite, TypeSec2).
