6d4ace316e72e01c7904065e044eb4f8c9faa1db
nicola169u <61577365+nicola169u@users.noreply.github.com>
2024-05-08 16:01:06 +0200

    Update prop.pl
    chemins atomiques ok
    connexions ok
    manque juste à adapter pour que ça fonctionne pour n'importe quel cas

--- prop.pl
+++ prop.pl
@@ -79,6 +79,8 @@ del(X, [Head|Tail], [Head|NewTail]) :-
 
 getNumero([Numero, _, _, _, _, _, _], Numero).
 
+estAtomique([Numero, _, _, _, _, nil, nil]).
+
 
 
 
@@ -95,93 +97,251 @@ chemins(Arbre) :-
 % Cas de base : feuille de l'arbre (proposition atomique)
 chemins(Noeud, [Numero, _, _, _, _, nil, nil], Final).
 
-% Cas pour les nœuds de type alpha
-chemins(Noeud, [Numero, alpha, _, _, _, Fils1, Fils2], Final) :-
-    writeln(Numero),
-    writeln(Noeud),
-    getNumero(Fils1, Num1),
-    getNumero(Fils2, Num2),
-    del(Numero, Noeud, PNoeud),
-    append(PNoeud, [Num1, Num2], NewNoeud),
-    write(NewNoeud),
-    write(" sous-ensemble obtenu à partir de "),
-    write(Numero),
-    nl,
-    (
-        futurFilsAtomique(Fils1)
-        -> 
-        chemins(NewNoeud, Fils2, Final1)
+chemins(Noeud, [Numero, alpha, _, _,_, Fils1, Fils2], Final):-
+    % writeln("JSUIS EN ALPHA"),
+    % writeln(Numero),
+    (Numero == 0 
+        ->
+        del(Numero, Noeud, PNoeud)
+        ;
+        del([Numero, alpha, _, _,_, Fils1, Fils2], Noeud, PNoeud)
     ),
+    append(PNoeud, [Fils1, Fils2], NewNoeud),
+    % writeln(NewNoeud),
+    taille(NewNoeud, Taille),
+    nth0(0, NewNoeud, Elem1),
+    nth0(1, NewNoeud, Elem2),
+    % writeln(Elem1),
+    % writeln(Elem2),
+    % getNumero(Elem1, Num1),
+    % getNumero(Elem2, Num2),
+    % write([Num1, Num2]),
+    % write(" sous-ensemble obtenu à partir de "),
+    % writeln(Numero), 
     (
-    futurFilsAtomique(Fils2)
-    -> 
-    chemins(NewNoeud, Fils1, Final1)
+        tousLesElementsAtomiques(NewNoeud) 
+        ->
+        write("Chemin atomique => "),
+        writeln(NewNoeud),
+        (\+verifierConnexion(NewNoeud)
+        ->
+        writeln("Connexion réussie pour ce noeud")
+        )
+        ;
+        (\+ estAtomique(Elem1), \+ estAtomique(Elem2)
+        ->
+            chemins(NewNoeud, Elem1, Final)
+            ;
+            (estAtomique(Elem1)
+            -> 
+            chemins(NewNoeud, Elem2, Final)
+            ;
+            chemins(NewNoeud, Elem1, Final)
+            )
+        )
+        
+
+    ).
+    
+
+
+chemins(Noeud, [Numero, beta, _, _,_, Fils1, Fils2], Final):-
+    % writeln("JE SUIS EN BETA"),
+    % writeln(Numero),
+    % writeln(Noeud),
+    del([Numero, beta, _, _,_, Fils1, Fils2], Noeud, PNoeud),
+    % writeln(PNoeud),
+    append(PNoeud, [Fils1], NewNoeud1),
+    append(PNoeud, [Fils2], NewNoeud2),
+    % writeln(NewNoeud1),
+    % writeln(NewNoeud2),
+    nth0(0, NewNoeud1, Elem1),
+    nth0(1, NewNoeud1, Elem2),
+    nth0(0, NewNoeud2, Elem3),
+    nth0(1, NewNoeud2, Elem4),
+    % writeln(Elem1),
+    % writeln(Elem2),
+    % writeln(Elem3),
+    % writeln(Elem4),
+    % getNumero(Elem1, Num1),
+    % getNumero(Elem2, Num2),
+    % getNumero(Elem3, Num3),
+    % getNumero(Elem4, Num4),
+    % write([Num1, Num2]),
+    % write(" Nouvelle branche à partir de "),
+    % writeln(Numero),
+    % write([Num3, Num4]),
+    % write(" Nouvelle branche à partir de "),
+    % writeln(Numero),
+    (
+        estAtomique(Fils1), estAtomique(FIls2)
+        ->
+        
+        (
+            \+estAtomique(Elem1), \+estAtomique(Elem3) ->
+            chemins(NewNoeud1, Elem1, Final),
+            chemins(NewNoeud2, Elem3, Final)
+            ;
+            chemins(NewNoeud1, Elem2, Final),
+            chemins(NewNoeud2, Elem4, Final)
+        )
+        
     ).
 
-    % ici aussi choisir quel fils mettre en fonction de s'il est atomique ou non car avec le type alpha on développe qu'une branche
-    % donc pas nécessaire d'appeler deux fois chemins (après c'est ce que je pense, je peux me tromper)
-    % (estAtomique(Fils1) ->
-    %     (
-    %         estAtomique(Fils2) ->
-    %         Final = NewNoeud
-    %         ;
-    %         chemins(NewNoeud, Fils2, Final1)
-    %     )
-    %     ;
-    %     chemins(NewNoeud, Fils1, Final1)
+    % (
+    %     estAtomique()
+    % )
+    % writeln(Elem1),
+    % writeln(Elem2),
+    % writeln(Elem3),
+    % writeln(Elem4).
+
+
+
+
+
+
+    % writeln(NoeudCourant).
+    % nth0(5, NoeudCourant, Fils),
+    % writeln(Fils).
+    
+
+% Prédicat pour calculer la taille d'une liste
+taille([], 0). % Cas de base : la taille d'une liste vide est 0.
+taille([_|Reste], Taille) :- taille(Reste, TailleReste), Taille is TailleReste + 1.
+
+tousLesElementsAtomiques([Elem|Reste]):-
+    estAtomique(Elem),
+    tousLesElementsAtomiques(Reste).
+
+tousLesElementsAtomiques([]).
+
+verifierConnexion([]).
+verifierConnexion([SousEnsemble|Reste]):-
+    % writeln(SousEnsemble),
+    % writeln(Reste),
+    compareNoeudAvecSousEnsemble(SousEnsemble, Reste, ConnexionFormee),
+    verifierConnexion(Reste),
+    
+    ( \+ ConnexionFormee ->
+        writeln("formule invalide."),
+        false % Si une connexion n'est pas formée dans un sous-ensemble, retourne false
+        ;
+        writeln("FORMULE VALIDE EH OH")
+    ).
+    
+    % ( ConnexionFormee ->
+    %     verifierConnexion(Reste)
+    % ;
+    %     writeln("formule invalide."),
+    %     false % Si une connexion n'est pas formée dans un sous-ensemble, retourne false
     % ).
 
-
-
-% Cas pour les nœuds de type beta
-chemins(Noeud, [Numero, beta, _, _, _, Fils1, Fils2], Final) :-
-    write("Je suis beta et le numero "),
-    write(Numero),
-    write(" et je recois "),
-    writeln(Noeud),
-    getNumero(Fils1, Num1),
-    getNumero(Fils2, Num2),
-    del(Numero, Noeud, PNoeud),
-    append(PNoeud, [Num1], NewNoeud1),
-    append(PNoeud, [Num2], NewNoeud2),
-    write(NewNoeud1),
-    write("   Nouvelle branche à partir de "),
-    write(" "),
-    write(Numero),
-    nl,
-    write(NewNoeud2),
-    write("   Nouvelle branche à partir de "),
-    write(" "),
-    write(Numero),
-    nl,
-    writeln(Fils1),
-    writeln(Fils2),
-    ( estAtomique(Fils1)
-    ->
-        write("il est atomique le "), 
-        writeln(Num1),
-        ( estAtomique(Fils2) -> 
-        write("il est atomique le "), 
-        writeln(Num2)
-        ;
-        write("il est pas atomique le "), 
-        writeln(Num2),
-        chemins(NewNoeud2, Fils2, Final2)
-        )
+compareNoeudAvecSousEnsemble(_, [], false).
+compareNoeudAvecSousEnsemble(Noeud, [Noeud2|Reste], ConnexionFormee):-
+    % writeln(Noeud),
+    % writeln(Noeud2),
+    % writeln(Reste),
+    ( formentConnexion(Noeud, Noeud2) ->
+        write('Connexion : '), writeln([Noeud, Noeud2]),
+        compareNoeudAvecSousEnsemble(Noeud, Reste, true)
     ;
-        write("il est pas atomique le "), 
-        writeln(Num1),
-        chemins(NewNoeud1, Fils1, Final1)
+        compareNoeudAvecSousEnsemble(Noeud, Reste, ConnexionFormee)
     ).
-    % chemins(NewNoeud1, Fils1, Final1),
-    % chemins(NewNoeud2, Fils2, Final2).
 
 
-estAtomique([_,_, _, _, _, nil,nil]).
+% Vérifie si deux nœuds forment une connexion
+formentConnexion([_, _, _, Polarite1, ValeurNoeud1, _, _], [_, _, _, Polarite2, ValeurNoeud2, _, _]) :-
+    Polarite1 \= Polarite2,
+    ValeurNoeud1 == ValeurNoeud2.
 
-futurFilsAtomique([Numero, _, _, _, _, Fils1, Fils2]) :- 
-    estAtomique(Fils1),
-    estAtomique(Fils2).
+% % Cas pour les nœuds de type alpha
+% chemins(Noeud, [Numero, alpha, _, _, _, Fils1, Fils2], Final) :-
+%     writeln(Numero),
+%     writeln(Noeud),
+%     getNumero(Fils1, Num1),
+%     getNumero(Fils2, Num2),
+%     del(Numero, Noeud, PNoeud),
+%     append(PNoeud, [Num1, Num2], NewNoeud),
+%     write(NewNoeud),
+%     write(" sous-ensemble obtenu à partir de "),
+%     write(Numero),
+%     nl,
+%     (
+%         futurFilsAtomique(Fils1)
+%         -> 
+%         chemins(NewNoeud, Fils2, Final1)
+%     ),
+%     (
+%     futurFilsAtomique(Fils2)
+%     -> 
+%     chemins(NewNoeud, Fils1, Final1)
+%     ).
+
+%     % ici aussi choisir quel fils mettre en fonction de s'il est atomique ou non car avec le type alpha on développe qu'une branche
+%     % donc pas nécessaire d'appeler deux fois chemins (après c'est ce que je pense, je peux me tromper)
+%     % (estAtomique(Fils1) ->
+%     %     (
+%     %         estAtomique(Fils2) ->
+%     %         Final = NewNoeud
+%     %         ;
+%     %         chemins(NewNoeud, Fils2, Final1)
+%     %     )
+%     %     ;
+%     %     chemins(NewNoeud, Fils1, Final1)
+%     % ).
+
+
+
+% % Cas pour les nœuds de type beta
+% chemins(Noeud, [Numero, beta, _, _, _, Fils1, Fils2], Final) :-
+%     write("Je suis beta et le numero "),
+%     write(Numero),
+%     write(" et je recois "),
+%     writeln(Noeud),
+%     getNumero(Fils1, Num1),
+%     getNumero(Fils2, Num2),
+%     del(Numero, Noeud, PNoeud),
+%     append(PNoeud, [Num1], NewNoeud1),
+%     append(PNoeud, [Num2], NewNoeud2),
+%     write(NewNoeud1),
+%     write("   Nouvelle branche à partir de "),
+%     write(" "),
+%     write(Numero),
+%     nl,
+%     write(NewNoeud2),
+%     write("   Nouvelle branche à partir de "),
+%     write(" "),
+%     write(Numero),
+%     nl,
+%     writeln(Fils1),
+%     writeln(Fils2),
+%     ( estAtomique(Fils1)
+%     ->
+%         write("il est atomique le "), 
+%         writeln(Num1),
+%         ( estAtomique(Fils2) -> 
+%         write("il est atomique le "), 
+%         writeln(Num2)
+%         ;
+%         write("il est pas atomique le "), 
+%         writeln(Num2),
+%         chemins(NewNoeud2, Fils2, Final2)
+%         )
+%     ;
+%         write("il est pas atomique le "), 
+%         writeln(Num1),
+%         chemins(NewNoeud1, Fils1, Final1)
+%     ).
+%     % chemins(NewNoeud1, Fils1, Final1),
+%     % chemins(NewNoeud2, Fils2, Final2).
+
+
+% estAtomique([_,_, _, _, _, nil,nil]).
+
+% futurFilsAtomique([Numero, _, _, _, _, Fils1, Fils2]) :- 
+%     estAtomique(Fils1),
+%     estAtomique(Fils2).
 
 % % Cas pour les nœuds de type alpha
 % chemins(Noeud, [Numero, alpha, _, _, _, Fils1, Fils2], Acc, Resultats) :-
@@ -237,10 +397,7 @@ eliminerDoublons([X|Reste], Acc, ListeSansDoublons) :-
 
 
 
-% Vérifie si deux nœuds forment une connexion
-formentConnexion([_, _, _, Polarite1, ValeurNoeud1, _, _], [_, _, _, Polarite2, ValeurNoeud2, _, _]) :-
-    Polarite1 \= Polarite2,
-    ValeurNoeud1 == ValeurNoeud2.
+
 
 % Vérifie chaque paire de nœuds dans un sous-ensemble
 verifierConnexions([], _).
@@ -262,6 +419,7 @@ comparerNoeudAvecSousEnsemble(Noeud, [Noeud2|Reste], ConnexionFormee) :-
 % Vérifie chaque sous-ensemble de CheminsAtomiques
 verifierCheminsAtomiques([]).
 verifierCheminsAtomiques([SousEnsemble|Reste]) :-
+    writeln(Reste),
     comparerNoeudAvecSousEnsemble(_, SousEnsemble, ConnexionFormee),
     ( ConnexionFormee ->
         verifierCheminsAtomiques(Reste)
